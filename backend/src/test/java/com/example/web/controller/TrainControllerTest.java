fe7a2164a20c87dc84d7255e28a33173f4941c4c
ferretdayo <ferretdayo@hotmail.co.jp>
2017-10-14 16:35:59 +0900

    hoge

---
+++ backend/src/test/java/com/example/web/controller/TrainControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.web.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrainControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
