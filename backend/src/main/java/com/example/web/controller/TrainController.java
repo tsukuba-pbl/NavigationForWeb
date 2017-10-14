f0d129133803f0f4e1d3f914e57acd1569828aaa
ferretdayo <ferretdayo@hotmail.co.jp>
2017-10-14 13:39:34 +0900

    dekita

--- backend/src/main/java/com/example/web/controller/TrainController.java
+++ backend/src/main/java/com/example/web/controller/TrainController.java
@@ -1,18 +1,27 @@
 package com.example.web.controller;
 
 import org.springframework.ui.Model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/api")
-public class IndexController {
+@RequestMapping("/api/train")
+public class TrainController {
 
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET)
-	public String index(Model model) {
-		return "index";
+	public Map<String, Integer> index(Model model) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("ume", 25);
+		map.put("turu", 23);
+		return map;
 	}
 }
