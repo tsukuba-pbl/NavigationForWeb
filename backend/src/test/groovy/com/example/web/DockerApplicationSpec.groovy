5fc3d4f81b260894083e2d6ef14489029983a7cc
ferretdayo <ferretdayo@hotmail.co.jp>
2017-10-15 02:33:22 +0900

    add spock

---
+++ backend/src/test/groovy/com/example/web/DockerApplicationSpec.groovy
@@ -0,0 +1,7 @@
+package com.example.web;
+
+import spock.lang.Specification
+
+public class DockerApplicationSpec extends Specification {
+
+}
---
+++ backend/src/test/groovy/com/example/web/controller/TrainControllerSpec.groovy
@@ -0,0 +1,7 @@
+package com.example.web.controller;
+
+import spock.lang.Specification
+
+public class TrainControllerSpec extends Specification {
+
+}
