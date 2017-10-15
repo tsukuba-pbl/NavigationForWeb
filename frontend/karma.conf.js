8421dee2e91d2a7095ec5009bc5cc991cb106674
ferretdayo <ferretdayo@hotmail.co.jp>
2017-10-15 15:52:17 +0900

    hogehoge

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
@@ -28,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS', 'Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: true
   });
 };
