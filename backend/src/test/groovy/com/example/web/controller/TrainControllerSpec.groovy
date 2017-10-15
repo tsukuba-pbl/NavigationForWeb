0d7e3bd6036378e027bb1b23fa318bccf49a955b
ferretdayo <ferretdayo@hotmail.co.jp>
2017-10-15 14:37:03 +0900

    can execute spock test and karma test

--- backend/src/test/groovy/com/example/web/controller/TrainControllerSpec.groovy
+++ backend/src/test/groovy/com/example/web/controller/TrainControllerSpec.groovy
@@ -3,5 +3,19 @@ package com.example.web.controller;
 import spock.lang.Specification
 
 public class TrainControllerSpec extends Specification {
-
+	private TrainController instance;
+	
+	def setup() {
+		instance = new TrainController()
+	}
+	
+	def "index"() {
+		expect:
+			instance.sum(a, b) == result
+			
+		where:
+		a | b || result
+		1 | 1 || 2
+		2 | 3 || 3
+	}
 }
