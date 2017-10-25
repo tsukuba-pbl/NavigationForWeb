9099154266b948e2f165a3c8bb70b9e58a212ec2
ferretdayo <ferretdayo@hotmail.co.jp>
2017-10-25 18:21:23 +0900

    a

--- backend/sonar.sh
+++ backend/sonar.sh
@@ -8,4 +8,4 @@ if [ $PULLS_NUMBER = 'false' ]; then
     exit
 fi
 
-mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dmaven.test.skip=true -Dclirr=true -Dsonar.host.url=https://sonarqube.com -Dsonar.login=${SONAR_TOKEN} -Dsonar.sourceEncoding=UTF-8 -Dsonar.analysis.mode=issues -Dsonar.github.pullRequest=${PULLS_NUMBER} -Dsonar.github.oauth=${GITHUB_OAUTH} -Dsonar.github.repository=tsukuba-pbl/NavigationForWeb -B -e -V
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dmaven.test.skip=true -Dclirr=true -Dsonar.host.url=https://sonarqube.com -Dsonar.login=${SONAR_TOKEN} -Dsonar.sourceEncoding=UTF-8 -Dsonar.analysis.mode=issues -Dsonar.github.pullRequest=${PULLS_NUMBER} -Dsonar.github.oauth=${GITHUB_OAUTH} -Dsonar.github.repository=tsukuba-pbl/NavigationForWeb
