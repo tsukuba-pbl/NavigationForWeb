920537345c5bf8bdf0e206c88ed4aa8fa9e61abc
ferretdayo <ferretdayo@hotmail.co.jp>
2017-11-15 16:05:33 +0900

    add event detail

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes }   from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-import { EventRegistrationComponent } from './event-registration/event-registration.component';
-import { IndexComponent } from './index/index.component';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes }   from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpModule } from '@angular/http'
 
 import { 
   MatIconModule, 
@@ -20,17 +16,23 @@ import {
   MatTabsModule,
 } from '@angular/material';
 
+import { AppComponent } from './app.component'
+import { IndexComponent } from './index/index.component'
+import { EventRegistrationComponent } from './event-registration/event-registration.component'
+import { EventDetailComponent } from './event-detail/event-detail.component';
+
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'event/new',  component: EventRegistrationComponent },
+  { path: 'event/:id', component: EventDetailComponent }
 ];
-
 
 @NgModule({
   declarations: [
     AppComponent,
     EventRegistrationComponent,
-    IndexComponent
+    IndexComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule,
