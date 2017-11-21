bae96af950710664e9c154e697ada6449caba122
Ikuzus-k <ae14suzuki@g.kumamoto-nct.ac.jp>
2017-11-21 13:43:01 +0900

    RouteEntityを作ったよ

---
+++ backend/src/main/java/com/example/web/entity/RouteEntity.java
@@ -0,0 +1,27 @@
+package com.example.web.entity;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class RouteEntity implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private int sourceId;
+	private int destinationId;
+	private String eventId;
+	
+	public class RouteRowMapper implements RowMapper<RouteEntity> {
+
+		@Override
+		public RouteEntity mapRow(ResultSet arg0, int arg1) throws SQLException {
+			return null;
+		}
+		
+	}
+}
