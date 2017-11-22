64522223f8658b958713aa5e3def5ebddaac0a67
ferretdayo <ferretdayo@hotmail.co.jp>
2017-11-22 15:16:44 +0900

    remove code

--- backend/src/main/java/com/example/web/CustomDeserializer.java
+++ backend/src/main/java/com/example/web/CustomDeserializer.java
@@ -4,13 +4,9 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.boot.json.GsonJsonParser;
-
-import com.example.web.entity.BeaconEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CustomDeserializer {
 	
@@ -30,22 +26,6 @@ public class CustomDeserializer {
 			} catch (java.text.ParseException e) {
 				throw new RuntimeException(e);
 			}
-		}
-	}
-
-	/**
-	 * RequestのJSONのDateフォーマットをStringからDateに変換
-	 * @author tomohiro
-	 *
-	 */
-	public static class BeaconsDeserializer extends JsonDeserializer<BeaconEntity[]> {
-		@Override
-		public BeaconEntity[] deserialize(com.fasterxml.jackson.core.JsonParser jsonParser, DeserializationContext deserializationContext)
-				throws IOException, JsonProcessingException {
-			ObjectMapper mapper = new ObjectMapper();
-	        String beacons = jsonParser.getText();
-	        BeaconEntity[] beaconList = mapper.readValue(beacons, BeaconEntity[].class);
-	        return beaconList;
 		}
 	}
 }
--- backend/src/main/java/com/example/web/entity/BeaconEntity.java
+++ backend/src/main/java/com/example/web/entity/BeaconEntity.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class BeaconEntity {
-
 	private int rssi;
 	private int minorId;
 }
