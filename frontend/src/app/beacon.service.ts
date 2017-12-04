76126be8ae1b7994683630e494a0d7c6b4447efb
Ikuzus-k <ae14suzuki@g.kumamoto-nct.ac.jp>
2017-12-04 16:08:55 +0900

    beaconRegistrationのHTMLとbeacon.serviceの作成

---
+++ frontend/src/app/beacon.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { ServiceBase } from './service-base'
+import { Http, Response } from '@angular/http'
+import { Observable } from 'rxjs/Rx'
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class BeaconService {
+
+  constructor() { }
+
+}
