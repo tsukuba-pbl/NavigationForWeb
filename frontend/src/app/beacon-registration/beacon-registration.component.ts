ab9d6deb426a2faa6444e68805c92616effb0ddf
Ikuzus-k <ae14suzuki@g.kumamoto-nct.ac.jp>
2017-12-04 17:01:30 +0900

    beaconRegistrationComponentを書いた

--- frontend/src/app/beacon-registration/beacon-registration.component.ts
+++ frontend/src/app/beacon-registration/beacon-registration.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { BeaconService } from '../beacon.service'
 
 @Component({
   selector: 'app-beacon-registration',
   templateUrl: './beacon-registration.component.html',
-  styleUrls: ['./beacon-registration.component.css']
+  styleUrls: ['./beacon-registration.component.css'],
+  providers: [BeaconService]
 })
 export class BeaconRegistrationComponent implements OnInit {
-
-  constructor() { }
+  beaconForm = new FormGroup({
+    beaconId: new FormControl('', [Validators.required]),
+  })
+  @ViewChild('f') form
+  eventId: string = ""
+  resultText: string = ""
+  beacons: BeaconType[] = []
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private beaconService: BeaconService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.eventId = params["id"]
+      this.beaconService.getBeacons(this.eventId)
+      .subscribe(result => {
+        this.beacons = result["beacons"]
+      })
+    })
   }
 
+  onSubmit(formGroup: FormGroup) {
+    if(!formGroup.valid || !this.eventId) {
+      return
+    }
+    const requestParam = formGroup.value
+
+    let requestBody: BeaconType = {
+      eventid: this.eventId,
+      minorid: requestParam.minorid,
+    }
+    this.beaconService.createBeacon(this.eventId, requestBody)
+    .subscribe(result => {
+      switch(result.status) {
+        case 200:
+          this.resultText = "ビーコンが登録できました"
+          this.form.resetForm()
+          this.beaconService.getBeacons(this.eventId)
+          .subscribe(result => {
+            this.beacons = result["beacons"]
+          })
+          break;
+        case 300:
+          this.resultText = "既に同じビーコンが登録されています"
+          break;
+        default:
+          this.resultText = "問題が発生したようです😥．．．"
+          break;
+      }
+    },
+      error => this.resultText = <any>error
+    )
+  }
 }
