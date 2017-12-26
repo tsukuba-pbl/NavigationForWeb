52fd3fa3a4704519eb0c52b6cd487199555b3df7
ferretdayo <ferretdayo@hotmail.co.jp>
2017-12-26 12:50:08 +0900

    timezoneの修正

--- backend/src/main/java/com/example/web/CustomDeserializer.java
+++ backend/src/main/java/com/example/web/CustomDeserializer.java
@@ -3,6 +3,7 @@ package com.example.web;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -20,6 +21,7 @@ public class CustomDeserializer {
 		public Date deserialize(com.fasterxml.jackson.core.JsonParser jsonParser, DeserializationContext deserializationContext)
 				throws IOException, JsonProcessingException {
 			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+			format.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"));
 	        String date = jsonParser.getText();
 	        try {
 				return format.parse(date);
