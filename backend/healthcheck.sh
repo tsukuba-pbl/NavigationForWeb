3aabcb54e3563882d8edcf740c0a790113a8fda9
ferretdayo <ferretdayo@hotmail.co.jp>
2018-02-07 16:25:52 +0900

    healthcheck

---
+++ backend/healthcheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until nc -vz "$host" 3306; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "MySQL is up - executing command"
+exec $cmd
